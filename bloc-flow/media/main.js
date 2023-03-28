eaab91fc3057fb40e29c8675f98719bdd9254991
DoanHo <doanho.it.dev@gmail.com>
2023-03-29 00:02:24 +0700

    parse-json

--- bloc-flow/media/main.js
+++ bloc-flow/media/main.js
@@ -3,101 +3,141 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
 (function () {
-    // @ts-ignore
-    const vscode = acquireVsCodeApi();
+  // @ts-ignore
+  const vscode = acquireVsCodeApi();
 
-    const oldState = vscode.getState() || { colors: [] };
+  const oldState = vscode.getState() || { colors: [] };
 
-    /** @type {Array<{ value: string }>} */
-    let colors = oldState.colors;
+  /** @type {Array<{ name: string, icon: string, content: string }>} */
+  let colors = oldState.colors;
 
-    updateColorList(colors);
+  updateColorList(colors);
 
-    // @ts-ignore
-    document.querySelector('.add-color-button').addEventListener('click', () => {
-        addColor();
+  // @ts-ignore
+  document.querySelector(".add-color-button").addEventListener("click", () => {
+    addColor({
+      content: "hihi",
+      icon: "https://lh3.googleusercontent.com/a-/AOh14GgofLnPE14e3bcXA6-v1W6dJWvs_uNSdLcvBqH8vQ=k-s256",
+      name: "sample",
     });
+  });
 
-    // Handle messages sent from the extension to the webview
-    window.addEventListener('message', event => {
-        const message = event.data; // The json data that the extension sent
-        switch (message.type) {
-            case 'addColor':
-                {
-                    addColor();
-                    break;
-                }
-            case 'clearColors':
-                {
-                    colors = [];
-                    updateColorList(colors);
-                    break;
-                }
-
-        }
-    });
-
-    /**
-     * @param {Array<{ value: string }>} colors
-     */
-    function updateColorList(colors) {
-        const ul = document.querySelector('.color-list');
-        // @ts-ignore
-        ul.textContent = '';
-        for (const color of colors) {
-            const li = document.createElement('li');
-            li.className = 'color-entry';
-
-            const colorPreview = document.createElement('div');
-            colorPreview.className = 'color-preview';
-            colorPreview.style.backgroundColor = `#${color.value}`;
-            colorPreview.addEventListener('click', () => {
-                onColorClicked(color.value);
-            });
-            li.appendChild(colorPreview);
-
-            const input = document.createElement('input');
-            input.className = 'color-input';
-            input.type = 'text';
-            input.value = color.value;
-            input.addEventListener('change', (e) => {
-                // @ts-ignore
-                const value = e.target.value;
-                if (!value) {
-                    // Treat empty value as delete
-                    colors.splice(colors.indexOf(color), 1);
-                } else {
-                    color.value = value;
-                }
-                updateColorList(colors);
-            });
-            li.appendChild(input);
-
-            // @ts-ignore
-            ul.appendChild(li);
-        }
-
-        // Update the saved state
-        vscode.setState({ colors: colors });
-    }
-
-    /** 
-     * @param {string} color 
-     */
-    function onColorClicked(color) {
-        vscode.postMessage({ type: 'colorSelected', value: color });
-    }
-
-    /**
-     * @returns string
-     */
-    function getNewCalicoColor() {
-        const colors = ['020202', 'f1eeee', 'a85b20', 'daab70', 'efcb99'];
-        return colors[Math.floor(Math.random() * colors.length)];
-    }
-
-    function addColor() {
-        colors.push({ value: getNewCalicoColor() });
+  // Handle messages sent from the extension to the webview
+  window.addEventListener("message", (event) => {
+    const message = event.data; // The json data that the extension sent
+    switch (message.type) {
+      case "addColor": {
+        addColor(message);
+        break;
+      }
+      case "clearColors": {
+        colors = [];
         updateColorList(colors);
+        break;
+      }
     }
-}());
+  });
+
+  /**
+   * @param {Array<{ name: string, icon: string, content: string }>} colors
+   */
+  function updateColorList(colors) {
+    const ul = document.querySelector(".color-list");
+    // @ts-ignore
+    ul.textContent = "";
+    for (const color of colors) {
+      const li = document.createElement("li");
+      li.className = "color-entry";
+
+      const colorPreview = document.createElement("div");
+      colorPreview.className = "color-preview";
+
+      colorPreview.style.backgroundColor = `#00ff00`;
+      colorPreview.addEventListener("click", () => {
+        onColorClicked(color.name);
+      });
+
+      const img = document.createElement("img");
+      img.className = "color-img";
+      img.src =
+        "https://lh3.googleusercontent.com/a-/AOh14GgofLnPE14e3bcXA6-v1W6dJWvs_uNSdLcvBqH8vQ=k-s256"; //color.icon;
+      img.alt = "image load";
+      img.width = 200;
+      img.height = 100;
+      img.addEventListener("change", (e) => {
+        // @ts-ignore
+        const value = e.target.value;
+        if (!value) {
+          // Treat empty value as delete
+          colors.splice(colors.indexOf(color), 1);
+        } else {
+          color.content = value;
+        }
+        updateColorList(colors);
+      });
+      const img2 = document.createElement("img");
+    //   img2.className = "color-img2";
+      img2.src =
+        "url(https://lh3.googleusercontent.com/a-/AOh14GgofLnPE14e3bcXA6-v1W6dJWvs_uNSdLcvBqH8vQ=k-s256)"; //color.icon;
+      img2.alt = "image load";
+      img2.width = 200;
+      img2.height = 100;
+      img2.addEventListener("change", (e) => {
+        // @ts-ignore
+        const value = e.target.value;
+        if (!value) {
+          // Treat empty value as delete
+          colors.splice(colors.indexOf(color), 1);
+        } else {
+          color.content = value;
+        }
+        updateColorList(colors);
+      });
+      
+      colorPreview.appendChild(img);
+
+      li.appendChild(colorPreview);
+      li.append(img2);
+
+      const input = document.createElement("input");
+      input.className = "color-input";
+      input.type = "text";
+      input.value = color.icon;
+      input.addEventListener("change", (e) => {
+        // @ts-ignore
+        const value = e.target.value;
+        if (!value) {
+          // Treat empty value as delete
+          colors.splice(colors.indexOf(color), 1);
+        } else {
+          color.content = value;
+        }
+        updateColorList(colors);
+      });
+      li.appendChild(input);
+
+      li.appendChild(document.createElement("br"));
+
+      // @ts-ignore
+      ul.appendChild(li);
+    }
+
+    // Update the saved state
+    vscode.setState({ colors: colors });
+  }
+
+  /**
+   * @param {string} color
+   */
+  function onColorClicked(color) {
+    vscode.postMessage({ type: "colorSelected", value: color });
+  }
+
+  /**
+   * @param {{ name: string, icon: string, content: string }} data
+   */
+  function addColor(data) {
+    colors.push({ ...data });
+    updateColorList(colors);
+  }
+})();
--- bloc-flow/src/extension.ts
+++ bloc-flow/src/extension.ts
@@ -3,6 +3,7 @@ import { newPage } from "./commans/new-page.commands";
 import * as path from "path";
 import * as fs from "fs";
 import { Uri } from "vscode";
+import fetch from "node-fetch";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "bloc-flow" is now active!');
@@ -26,17 +27,23 @@ export function activate(context: vscode.ExtensionContext) {
   const provider = new ColorsViewProvider(context.extensionUri);
 
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
+    vscode.window.registerWebviewViewProvider(
+      ColorsViewProvider.viewType,
+      provider
+    )
+  );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('bloc-flow.addColor', () => {
+    vscode.commands.registerCommand("bloc-flow.addColor", () => {
       provider.addColor();
-    }));
+    })
+  );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('bloc-flow.clearColors', () => {
+    vscode.commands.registerCommand("bloc-flow.clearColors", () => {
       provider.clearColors();
-    }));
+    })
+  );
 }
 
 export function deactivate() { }
@@ -147,9 +154,8 @@ export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
 export class Dependency extends vscode.TreeItem {
   constructor(
     public readonly label: string,
-    private readonly version: string // public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-  ) // public readonly command?: vscode.Command
-  {
+    private readonly version: string // public readonly collapsibleState: vscode.TreeItemCollapsibleState, // public readonly command?: vscode.Command
+  ) {
     super(label /*, collapsibleState*/);
 
     this.tooltip = `${this.label}-${this.version}`;
@@ -170,19 +176,24 @@ export class Dependency extends vscode.TreeItem {
 }
 
 class ColorsViewProvider implements vscode.WebviewViewProvider {
-
-  public static readonly viewType = 'bloc-flow.colorsView';
+  public static readonly viewType = "bloc-flow.colorsView";
 
   private _view?: vscode.WebviewView;
 
-  constructor(
-    private readonly _extensionUri: vscode.Uri,
-  ) { }
+  constructor(private readonly _extensionUri: vscode.Uri) {
+    fetch('https://raw.githubusercontent.com/DoanpPhiHo/vs_bloc_extension/master/bloc-flow/assets/data.json')
+      .then((value) => value.json()).then((value) => {
+        for (let i = 0; i < value.code.length; i++) {
+          console.log(value.code[i]);
+          this.addColor2(value.code[i]);
+        }
+      });
+  }
 
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
     context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken,
+    _token: vscode.CancellationToken
   ) {
     this._view = webviewView;
 
@@ -190,20 +201,19 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
       // Allow scripts in the webview
       enableScripts: true,
 
-      localResourceRoots: [
-        this._extensionUri
-      ]
+      localResourceRoots: [this._extensionUri],
     };
 
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-    webviewView.webview.onDidReceiveMessage(data => {
+    webviewView.webview.onDidReceiveMessage((data) => {
       switch (data.type) {
-        case 'colorSelected':
-          {
-            vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-            break;
-          }
+        case "colorSelected": {
+          vscode.window.activeTextEditor?.insertSnippet(
+            new vscode.SnippetString(`#${data.value}`)
+          );
+          break;
+        }
       }
     });
   }
@@ -211,24 +221,38 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
   public addColor() {
     if (this._view) {
       this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
-      this._view.webview.postMessage({ type: 'addColor' });
+      this._view.webview.postMessage({ type: "addColor" });
+    }
+  }
+  public addColor2(data: { name: string, icon: string, content: string }) {
+    if (this._view) {
+      this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+      this._view.webview.postMessage({ type: "addColor", ...data });
     }
   }
 
   public clearColors() {
     if (this._view) {
-      this._view.webview.postMessage({ type: 'clearColors' });
+      this._view.webview.postMessage({ type: "clearColors" });
     }
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
     // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
 
     // Do the same for the stylesheet.
-    const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-    const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
 
     // Use a nonce to only allow a specific script to be run.
     const nonce = getNonce();
@@ -250,6 +274,7 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
 				<title>Cat Colors</title>
 			</head>
 			<body>
+      <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
 				<ul class="color-list">
 				</ul>
 				<button class="add-color-button">Add Color</button>
@@ -260,8 +285,9 @@ class ColorsViewProvider implements vscode.WebviewViewProvider {
 }
 
 function getNonce() {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 32; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
