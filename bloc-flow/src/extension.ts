02a10cd3a584a91f8d96f186d3f37ca8ca148515
HoDoan <doanphj5@gmail.com>
2023-03-28 12:17:31 +0700

    update-func-new-page

--- bloc-flow/src/extension.ts
+++ bloc-flow/src/extension.ts
@@ -1,15 +1,14 @@
 import * as vscode from "vscode";
+import { newPage } from "./commans/new-page.commands";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "bloc-flow" is now active!');
-  let disposable = vscode.commands.registerCommand("bloc-flow.new_page", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from bloc_flow!");
-  });
+  let disposable = vscode.commands.registerCommand(
+    "bloc-flow.new_page",
+    newPage
+  );
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
