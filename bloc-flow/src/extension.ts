d0b4f1bc41c4c91b9061fee03ac49161e861cd7a
DoanHo <doanho.it.dev@gmail.com>
2023-07-29 16:48:32 +0700

    https://marketplace.visualstudio.com/manage/publishers/HoDoan/extensions/bloc-flow/hub

--- bloc-flow/src/extension.ts
+++ bloc-flow/src/extension.ts
@@ -5,12 +5,14 @@ import { newBloc } from "./commands/new-bloc.commands";
 import { newModel } from "./commands/new-model.commands";
 import { newCore } from "./commands/new-core.commands";
 import { newColor } from "./commands/new-color.commands";
+import { newPageMVC } from "./commands/new-page-mvc.commands";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "bloc-flow" is now active!');
 
   context.subscriptions.push(
     vscode.commands.registerCommand("bloc-flow.new_feature", newPage),
+    vscode.commands.registerCommand("bloc-flow.new_feature_bloc_mvc", newPageMVC),
     vscode.commands.registerCommand(
       "bloc-flow.new_presentation",
       newPresentation
